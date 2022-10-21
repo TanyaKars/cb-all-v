19edac83c99a610d8388ba3f2a00a6e41183f6e0
tanyakars <euphratus.tk@gmail.com>
2022-10-21 14:34:56 -0700

    install ts

---
+++ cypress/support/e2e.ts
@@ -0,0 +1,13 @@
+import './commands'
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            /**
+             * Custom command to select DOM element by data-cy attribute.
+             * @example cy.dataCy('greeting')
+             */
+            dataCy(value: string): Chainable<Element>
+        }
+    }
+}
